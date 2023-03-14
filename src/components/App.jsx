f1f64bdc21b7fb96085f3f8202764f9d186200e4
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-03-14 14:36:17 +0200

    add Contact list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,17 @@ import style from './App.module.css';
 import { nanoid } from 'nanoid'
 
 import ContactForm from './ContactForm';
+import ContactList from './ContactList/ContactList';
 
 class App extends Component {
 
 state = {
-  contacts: [],
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
   filter: ''
 }
 
@@ -23,12 +29,32 @@ addContact = ({ name, number }) => {
       contacts: [...prevState.contacts, contact],
     }));
   }; 
+
+  deleteContact = todoId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== todoId),
+    }));
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   
   render() {
+    const visibleContacts = this.getVisibleContacts();
+    
     return (
       <div className={style.container}> 
        <h1>Phonebook</h1>  
         <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <ContactList onDeleteContact={this.deleteContact} contacts={visibleContacts}/>
       </div>)
 }
 }
